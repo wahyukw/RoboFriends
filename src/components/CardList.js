0a8d3a37a13f8748833ab13b294c1b60429371a6
Wahyu Kwan <wahyukwan@gmail.com>
2021-09-29 10:41:14 +0700

    adding files

---
+++ src/components/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => { 
+    return(
+        <div>
+            {robots.map((user,i) => {
+                return <Card 
+                        id={robots[i].id}
+                        name={robots[i].name}
+                        email={robots[i].email}
+                        key={i} />
+            })}
+        </div>
+    );
+}
+export default CardList;
